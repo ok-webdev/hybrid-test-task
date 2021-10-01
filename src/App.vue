d9c98e5ac1bfceb1f6bdd3ecb093275841489dc8
Oleg Kokoshinskii <oleg.kokoshinskii@gmail.com>
2021-10-01 17:11:14 +0200

    add TheHeader

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,30 @@
 <template>
   <div id="app">
-    
+    <the-header 
+      title="Hybrid тестовое задание"
+    />
   </div>
 </template>
 
 <script>
-
+import TheHeader from './components/ui/TheHeader'
 export default {
   name: 'App',
   components: {
-    
+    TheHeader
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  
-}
+  *,
+  *::before,
+  *::after {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  #app {
+    
+  }
 </style>
---
+++ src/components/ui/TheHeader.vue
@@ -0,0 +1,29 @@
+<template>
+  <header class="header">
+    <h1 class="header__title">
+      {{title}}
+    </h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  props: {
+    title: {
+      type: String,
+      required: true,
+      default: 'Header'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .header{ 
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 20px;
+  }
+</style>
