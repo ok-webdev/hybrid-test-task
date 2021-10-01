3353b8624a4f15413b427a8d7ad00f7421b59a7c
Oleg Kokoshinskii <oleg.kokoshinskii@gmail.com>
2021-10-01 17:53:30 +0200

    add header styles

--- src/components/ui/TheHeader.vue
+++ src/components/ui/TheHeader.vue
@@ -24,6 +24,12 @@ export default {
     display: flex;
     align-items: center;
     justify-content: center;
+    background: #FFFFFF;
+    box-shadow: 0px 6px 18px rgba(0, 0, 0, 0.06);
     padding: 20px;
+    &__title{
+      font-size: 36px;
+      font-weight: 500;
+    }
   }
 </style>
