dbd23e75496be7035d4e0efeec8487b8e712b5e2
Oleg Kokoshinskii <oleg.kokoshinskii@gmail.com>
2021-10-02 17:48:58 +0200

    add onClick method and styles for button

--- src/components/ui/BaseButton.vue
+++ src/components/ui/BaseButton.vue
@@ -2,6 +2,7 @@
   <button 
     class="button"
     :type="type"
+    @click="onClick"
   >
     {{name}}
   </button>
@@ -21,12 +22,21 @@ export default {
       required: false,
       default: 'submit'
     }
-  }  
+  },
+  methods: {
+    onClick (event) {
+      event.preventDefault();
+      this.$emit('click');
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
   .button{
+    display: flex;
+    align-items: center;
+    justify-content: center;
     background: #fcfcfc;
     font-size: 16px;
     font-weight: 500;
@@ -36,12 +46,18 @@ export default {
     border-radius: 5px;
     box-shadow: 0px -6px 10px 0px rgba(0, 0, 0, 0.04) inset, 
                 0px 6px 10px rgba(0, 0, 0, 0.09);
-    padding: 20px 30px;
+    padding: 10px 35px;
     outline: transparent;
     cursor: pointer;
-    &:hover{
-      box-shadow: 0px -6px 12px 0px rgba(0, 0, 0, 0.06) inset, 
-                0px 6px 15px rgba(0, 0, 0, 0.12);
+    transition: all .3s;
+    &:hover,
+    &:focus{
+      box-shadow: 0px -6px 12px 3px rgba(0, 0, 0, 0.06) inset, 
+                  0px 6px 15px rgba(0, 0, 0, 0.12);
+    }
+    &:active {
+      background: #f7f7f7;
+      box-shadow: -2px -2px 8px 8px rgba(0, 0, 0, 0.12) inset;
     }
   }
 </style>
