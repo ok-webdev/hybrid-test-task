7cbd6e2b7c53800913d7752c3b7a5a5e19661c7b
Oleg Kokoshinskii <oleg.kokoshinskii@gmail.com>
2021-10-02 17:51:45 +0200

    add changes to App

--- src/App.vue
+++ src/App.vue
@@ -2,30 +2,76 @@
   <div id="app">
     <the-header 
       title="Hybrid тестовое задание"
+      @open-modal="showModal"
     />
+    <div class="content">
+      <card-post 
+        v-for="post in posts"
+        :key="post.date"
+        :date="post.date"
+      />
+    </div>
+    <modal-backdrop v-if="isModal" @close="hideModal">
+      <add-post @close="hideModal"/>
+    </modal-backdrop>
   </div>
 </template>
 
 <script>
-import TheHeader from './components/ui/TheHeader'
+import TheHeader from './components/ui/TheHeader';
+import CardPost from './components/blog/CardPost';
+import AddPost from './components/modals/AddPost';
+import ModalBackdrop from './components/ui/ModalBackdrop';
+
 export default {
   name: 'App',
   components: {
-    TheHeader
+    TheHeader,
+    CardPost,
+    AddPost,
+    ModalBackdrop
+  },
+  data() {
+    return {
+      posts: [
+        {date: new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '')},
+        {date: new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '')},
+        {date: new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '')},
+        {date: new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '')},
+      ],
+      isModal: false,
+    }
+  },
+  methods: {
+    showModal() {
+      this.isModal = true;
+    },
+    hideModal () {
+      this.isModal = false;
+    }
   }
 }
 </script>
 
 <style lang="scss">
+  @import './assets/scss/normalize';
   @import './assets/scss/fonts';
   *,
   *::before,
   *::after {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
+    font-family: 'Roboto', sans-serif;
   }
   #app {
-    font-family: 'Roboto', sans-serif;
+    height: 100vh;
+    background: #fcfcfc;
+    position: relative;
+  }
+  .content{ 
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    grid-gap: 20px;
+    flex-wrap: wrap;
+    margin-top: 20px;
   }
 </style>
