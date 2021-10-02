95e245ab201e5bb7a5a0130d2158416ea3e1d4d2
Oleg Kokoshinskii <oleg.kokoshinskii@gmail.com>
2021-10-02 18:41:27 +0200

    add store

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
+import store from './store'
 
 // components
 import BaseButton from './components/ui/BaseButton';
@@ -9,5 +10,6 @@ Vue.component('base-button', BaseButton);
 Vue.config.productionTip = false
 
 new Vue({
+  store,
   render: h => h(App),
 }).$mount('#app')
