5766b23bcb87965d084fd0ab49c6d62d3f5a8574
Oleg Kokoshinskii <oleg.kokoshinskii@gmail.com>
2021-10-03 18:01:02 +0200

    add router

--- src/main.js
+++ src/main.js
@@ -1,15 +1,17 @@
-import Vue from 'vue'
-import App from './App.vue'
-import store from './store'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
 
 // components
 import BaseButton from './components/ui/BaseButton';
 
 Vue.component('base-button', BaseButton);
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   store,
+  router,
   render: h => h(App),
 }).$mount('#app')
