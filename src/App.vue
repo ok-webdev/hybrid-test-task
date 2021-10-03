eb2a9a222e5e4bd0308ce0be771f901132c907b8
Oleg Kokoshinskii <oleg.kokoshinskii@gmail.com>
2021-10-03 17:53:06 +0200

    add test card/ fix

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,7 @@
         :date="post.date"
         :title="post.title"
         :description="post.description"
+        :comments="post.comments.length"
       />
     </div>
     <modal-backdrop v-if="isModal" @close="hideModal">
@@ -38,7 +39,9 @@ export default {
   },
   data() {
     return {
-      posts: [],
+      posts: [
+        {date: new Date().toISOString().replace(/T/, ' ').replace(/\..+/, ''), title: 'Test post', description: 'Test description', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', comments: [{name: 'User', text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'}]}
+      ],
       isModal: false,
     }
   },
@@ -52,9 +55,10 @@ export default {
     addPostHandle(data) {
       const newPost = {
         date: new Date().toISOString().replace(/T/, ' ').replace(/\..+/, ''),
-        title: data.postContent,
+        title: data.postTitle,
         description: data.postDescription,
-        content: data.postTitle
+        content: data.postContent,
+        comments: []
       };
       this.posts.push(newPost);
       this.hideModal();
