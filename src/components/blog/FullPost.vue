e5bfb489693585803f1c0362226f9570f044dd11
Oleg Kokoshinskii <oleg.kokoshinskii@gmail.com>
2021-10-05 17:00:47 +0200

    add  FullPost

---
+++ src/components/blog/FullPost.vue
@@ -0,0 +1,37 @@
+<template>
+  <section class="post">
+    <header class="post__header">
+      <h1>{{ title }}</h1>
+      <p>{{ date }}</p>
+    </header>
+    <p>{{ content }}</p>
+    <p>{{ comments }}</p>
+  </section>
+</template>
+
+<script>
+  export default {
+    name: 'Post',
+    props: {
+      title: {
+        type: String,
+        required: false,
+        default: 'No title',
+      },
+      date: {
+        type: String,
+        requred: true,
+      },
+      content: {
+        type: String,
+        requred: false,
+        default: 'No content',
+      },
+      comments: {
+        type: Array,
+      },
+    },
+  };
+</script>
+
+<style lang="scss" scoped></style>
