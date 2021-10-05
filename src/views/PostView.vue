966dc7a107eda44f69ff3d79fef0662e1abb4159
Oleg Kokoshinskii <oleg.kokoshinskii@gmail.com>
2021-10-05 17:01:24 +0200

    add FullPost to PostView

--- src/views/PostView.vue
+++ src/views/PostView.vue
@@ -1,19 +1,42 @@
 <template>
   <div class="post">
-    <the-header 
-      title="Post view"
+    <the-header title="Post view" />
+    <full-post
+      :title="title"
+      :date="date"
+      :content="content"
+      :comments="comments"
     />
   </div>
 </template>
 
 <script>
-import TheHeader from '../components/ui/TheHeader'
+  import FullPost from '../components/blog/FullPost';
+  import TheHeader from '../components/ui/TheHeader';
 
-export default {
-  components: { TheHeader },
-}
+  export default {
+    name: 'PostView',
+    components: { TheHeader, FullPost },
+    data() {
+      return {
+        title: null,
+        date: null,
+        content: null,
+        comments: null,
+      };
+    },
+    mounted() {
+      const posts = JSON.parse(localStorage.getItem('posts'));
+      posts.filter((post) => {
+        if (post.id.toString() === this.$route.params.postId) {
+          this.title = post.title;
+          this.date = post.date;
+          this.content = post.content;
+          this.comments = post.comments;
+        }
+      });
+    },
+  };
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
