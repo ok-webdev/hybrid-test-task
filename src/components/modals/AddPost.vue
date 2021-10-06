82447ba64f2799515d10605fa72fddf21bafa1a3
Oleg Kokoshinskii <oleg.kokoshinskii@gmail.com>
2021-10-06 22:45:45 +0200

    add component instead of inputs and textarea

--- src/components/modals/AddPost.vue
+++ src/components/modals/AddPost.vue
@@ -3,7 +3,7 @@
     <form class="add-post__form">
       <div class="add-post__inputs">
         <div class="add-post__input">
-          <input
+          <form-input
             type="text"
             id="post-title"
             placeholder="enter title"
@@ -11,7 +11,7 @@
           />
         </div>
         <div class="add-post__input">
-          <input
+          <form-input
             type="text"
             id="post-description"
             placeholder="enter short description"
@@ -19,13 +19,11 @@
           />
         </div>
         <div class="add-post__input">
-          <textarea
+          <text-area
             id="post-content"
-            cols="30"
-            rows="10"
             placeholder="enter post"
             v-model="postContent"
-          ></textarea>
+          />
         </div>
       </div>
       <div class="add-post__buttons">
@@ -38,10 +36,14 @@
 
 <script>
   import BaseButton from '../ui/BaseButton';
+  import FormInput from '../ui/FormInput';
+  import TextArea from '../ui/TextArea';
   export default {
     name: 'AddPost',
     components: {
       BaseButton,
+      FormInput,
+      TextArea,
     },
     data() {
       return {
@@ -75,27 +77,10 @@
     padding: 20px;
     border-radius: 5px;
     &__input {
+      width: 100%;
       display: flex;
       flex-direction: column;
       margin-bottom: 20px;
-      input,
-      textarea {
-        font-size: 12px;
-        font-weight: 400;
-        color: #5c5c5c;
-        border: 1px solid rgba(0, 0, 0, 0.03);
-        border-radius: 5px;
-        box-shadow: 3px 3px 6px 2px rgba(0, 0, 0, 0.03) inset;
-        outline: transparent;
-        padding: 10px;
-        &:focus {
-          border: 1px solid rgba(0, 0, 0, 0.06);
-          box-shadow: 3px 3px 8px 2px rgba(0, 0, 0, 0.04) inset;
-        }
-      }
-      textarea {
-        resize: none;
-      }
     }
     &__buttons {
       display: flex;
