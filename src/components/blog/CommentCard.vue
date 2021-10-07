99143b6bd2511c4c9b1ebdc9374b09405f00fd3b
Oleg Kokoshinskii <oleg.kokoshinskii@gmail.com>
2021-10-07 19:46:19 +0200

    add styles for CommentCard

--- src/components/blog/CommentCard.vue
+++ src/components/blog/CommentCard.vue
@@ -10,14 +10,16 @@
       </p>
     </div>
     <footer class="comment__footer">
-      <a href="#" class="comment__update">update</a>
-      <a href="#" class="comment__delete">delete</a>
+      <base-button class="comment__delete" name="delete" />
+      <base-button class="comment__edit" name="edit" />
     </footer>
   </div>
 </template>
 
 <script>
+  import BaseButton from '../ui/BaseButton.vue';
   export default {
+    components: { BaseButton },
     name: 'CommentCard',
     props: {
       name: {
@@ -36,3 +38,52 @@
     },
   };
 </script>
+
+<style lang="scss" scoped>
+  .comment {
+    width: 30%;
+    background: #fcfcfc;
+    box-shadow: 0px -6px 10px 0px rgba(0, 0, 0, 0.04) inset,
+      0px 6px 20px rgba(0, 0, 0, 0.06);
+    padding: 30px;
+    transition: all 0.3s;
+    text-decoration: none;
+    margin-top: 10px;
+    margin-right: 10px;
+    &:hover {
+      box-shadow: 0px -6px 12px 0px rgba(0, 0, 0, 0.06) inset,
+        0px 6px 15px rgba(0, 0, 0, 0.1);
+    }
+    &__header {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+    }
+    &__name {
+      font-size: 14px;
+      font-weight: 400;
+      color: #474747;
+    }
+    &__date {
+      font-size: 12px;
+      font-weight: 400;
+      color: #8f8f8f;
+    }
+    &__text {
+      font-size: 14px;
+      font-weight: 400;
+      color: #474747;
+      line-height: 150%;
+      letter-spacing: 0.15px;
+    }
+    &__footer {
+      display: flex;
+      align-items: center;
+      justify-content: flex-start;
+      margin-top: 20px;
+    }
+    &__delete {
+      margin-right: 30px;
+    }
+  }
+</style>
